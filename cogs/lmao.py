b442a8e5d65a04d5c93d49f1843bd5ea3034a2a5
JT Finn <jtfinn0305@gmail.com>
2026-03-16 15:43:37 +0800

    Update lmao.py

--- cogs/lmao.py
+++ cogs/lmao.py
@@ -12,8 +12,6 @@ class Lmao(commands.Cog):
             return
 
         if message.content == "超好笑":
-            embed = discord.Embed(color=discord.Color.brand_red())
-            
             embed.set_image(url="https://imgur.com/gallery/up-1616-IYf0am5#/t/photo")
             
             await message.channel.send(embed=embed)
