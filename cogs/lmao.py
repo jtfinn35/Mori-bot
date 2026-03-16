6a5dddc6d967b3f8ec5d8fdbea835347e1586096
JT Finn <jtfinn0305@gmail.com>
2026-03-16 15:40:20 +0800

    Update lmao.py

--- cogs/lmao.py
+++ cogs/lmao.py
@@ -1,17 +1,22 @@
 import discord
 from discord.ext import commands
 
-class ReplyCog(commands.Cog):
+class Lmao(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        
         if message.author == self.bot.user:
             return
-          
+
         if message.content == "超好笑":
-            await message.channel.send("https://imgur.com/gallery/up-1616-IYf0am5#/t/photo")
+            embed = discord.Embed(color=discord.Color.brand_red())
+            
+            embed.set_image(url="https://imgur.com/gallery/up-1616-IYf0am5#/t/photo")
+            
+            await message.channel.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(ReplyCog(bot))
+    await bot.add_cog(Lmao(bot))
