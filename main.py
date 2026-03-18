ff20bf8acab5f2a1ddeff3417d5e28b2077e709b
JT Finn <jtfinn0305@gmail.com>
2026-03-18 11:03:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 import os
-from keep_alive 
-import keep_alive
+from keep_alive import keep_alive
 
 class MoriBot(commands.Bot):
     def __init__(self):
